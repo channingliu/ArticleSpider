025ca049fc97c8fd0a3cc8a3ffcae53c7ecee3f8
channingliu <542605150@qq.com>
2017-08-24 18:04:12 +0800

    更新jobbole.py

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+from scrapy.http import Request
+from urllib import parse #在py2.7中 用法：import urlparse
 
 
 class JobboleSpider(scrapy.Spider):
@@ -14,12 +16,42 @@ class JobboleSpider(scrapy.Spider):
         2.获取下一页的URL并交给scrapy进行下载，下载完成后交给parse
         '''
         #解析列表页中的所有文章的URL并交给scrapy下载后并进行解析
-        post_urls = response.css('#archive ')
+        #1.解析所有的URL
+        post_urls = response.css('#archive .floated-thumb .post-thumb a::attr(href)').extract()
         #post_urls = response.xpath('//div[@id="archive"]/div/div[2]/p/a[@target="_blank"]/@href').extract()
+        #2.下载URL
+        for post_url in post_urls:
+            yield Request(self,post_url,callback = self.parse_detail)
+            #有些网站的只有子域名，在提取的时候需要全域名（主域名+子域名）给parse。可用下面提供的方法：
+            #request(URL = parse.urljoin(response.url,post_url),callback = self.parse_detail)
+            print (post_url)
+            print (post_url)
 
+        #提取下一页，并给scrapy下载
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_url:
+            yield Request(self, post_url, callback=self.parse)
+
+    #parse的回调函数
+    def parse_detail(self,response):
+        #提取文章的目标内容
         title = response.xpath('//*[@id="post-112239"]/div[1]/h1/text()').extract()[0]
         create_time = response.xpath('// *[@id="post-112239"]/div[2]/p/text()[1]').extract()[0].strip().replace('·','').strip()
         # 使用extract_first()避免extract()[0]列表为空时报错
-        praise_nums = response.xpath('//*[@id="112239votetotal"]/text()').extract_first()
+        praise_nums = response.xpath('//*[@id="112239votetotal"]/text()').extract_first() #点赞
+        fav_nums = response.xpath('//*[@id="post-112219"]/div[3]/div[4]/span[2]/text()').extract()[0] #收藏
+        match_re = re.match(r".*?(\d+).*",fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+        else:
+            fav_nums = 0
+        #评论
+        comments_nums = response.xpath('//*[@id="post-112219"]/div[3]/div[4]/a/span/text()').extract()[0]
+        match_re1 = re.match(r".*?(\d+).*", comments_nums)
+        if comments_nums:
+            comments_nums = match_re1.group(1)
+        else:
+            comments_nums = 0
+
         tag = response.xpath('//*[@id="post-112239"]/div[2]/p/a[3]/text()').extract()[0]
         pass
