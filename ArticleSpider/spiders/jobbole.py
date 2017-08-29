b894e6afb0d97e7a6311f5aae407432edb6934df
channingliu <542605150@qq.com>
2017-08-30 07:56:23 +0800

    更新

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -20,13 +20,15 @@ class JobboleSpider(scrapy.Spider):
 
         # 解析列表页中的所有文章的URL并交给scrapy下载后并进行解析
         # 1.解析所有的URL
-        post_urls = response.css('#archive .floated-thumb .post-thumb a::attr(href)').extract()
+        post_nodes = response.css('#archive .floated-thumb .post-thumb a')
         # post_urls = response.xpath('//div[@id="archive"]/div/div[2]/p/a[@target="_blank"]/@href').extract()
         # 2.Request对获取的URL进行下载，在完成后调用自定义的解析函数
-        for post_url in post_urls:
-            yield Request(post_url, callback=self.parse_detail)
+        for post_node in post_nodes:
+            image_url = post_node.css("img::attr(src)").extract_first()
+            post_url = post_node.css("::attr(href)").extract_first()
+            # yield Request(post_url, callback=self.parse_detail)
             # 有些网站的只有子域名，在提取的时候需要全域名（主域名+子域名）给parse。可用下面提供的方法：
-            # Request(URL=parse.urljoin(response.url, post_url), callback=self.parse_detail)
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url": image_url}, callback=self.parse_detail)
             print(post_url)
 
         # 提取下一页，并给scrapy下载
@@ -36,11 +38,12 @@ class JobboleSpider(scrapy.Spider):
 
     # parse的回调函数，提取文章的目标内容
     def parse_detail(self, response):
+        front_image_url = response.meta.get("front_image_url", "")  # 文章封面图
         title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first()
         create_time = response.xpath('//p[@class="entry-meta-hide-on-mobile"]/text()[1]').extract()[0].strip().replace('·', '').strip()
         # 使用extract_first()避免extract()[0]列表为空时报错
-        praise_nums = response.css('h10::text').extract_first() # 点赞
-        fav_nums = response.css('.bookmark-btn::text').extract()[0] # 收藏
+        praise_nums = response.css('h10::text').extract_first()  # 点赞
+        fav_nums = response.css('.bookmark-btn::text').extract()[0]  # 收藏
         match_re = re.match(r".*?(\d+).*", fav_nums)
         if match_re:
             fav_nums = match_re.group(1)
